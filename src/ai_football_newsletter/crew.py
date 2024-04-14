0750bd8e4ecdbe16b6a7e504a17d3f1dac83239e
felixggj <me@felix-gomez.com>
2024-04-14 14:12:39 +0200

    perfected tasks istructions

--- src/ai_football_newsletter/crew.py
+++ src/ai_football_newsletter/crew.py
@@ -86,7 +86,7 @@ class FootballNewsletterCrew():
 			agents=self.agents, # Automatically created by the @agent decorator
 			tasks=self.tasks,
 			process=Process.hierarchical,
-			# manager_llm=OpenAIGPT3,
+			#manager_llm=OpenAIGPT3,
 			manager_llm=OpenAIGPT4,
 			verbose=2
 		)
